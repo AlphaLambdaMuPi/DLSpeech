9e6bd5007fe7879b88eb38fdc39e9d0af50a025e
STEP5 <kevin00050@gmail.com>
2015-03-24 17:25:18 +0800

    Read line

---
+++ codes/read_data.py
@@ -0,0 +1,64 @@
+import numpy as np
+
+def read_label(path, pmpath, datasize=1E9):
+    pmap = {}
+    for line in open(pmpath):
+        x = line.strip('\n').split()
+        pmap[x[0]] = int(x[1])
+
+    arr = []
+    data_count = 0
+
+    for line in open(path):
+        x = line.strip('\n').split(',')
+        arr.append(pmap[x[1]])
+        data_count += 1
+        if data_count >= datasize:
+            break
+
+    print('Label readed : {0}'.format(data_count))
+    return np.array(arr)
+
+def read_feature(path, datasize=1E9):
+    arr = []
+    data_count = 0
+    for line in open(path):
+        x = [float(a) for a in line.strip('\n').split()[1:]]
+        arr.append(x)
+        data_count += 1
+        if data_count >= datasize:
+            break
+
+    print('Feature readed : {0}'.format(data_count))
+    return np.array(arr)
+
+def sort_label(fpath, lpath, newlpath):
+    names = []
+    for line in open(fpath):
+        names.append(line.split()[0])
+    mp = {}
+    for line in open(lpath):
+        alp = line.strip('\n').split(',')
+        mp[alp[0]] = alp[1]
+    f = open(newlpath, 'w')
+    for nm in names:
+        f.write(nm + ',' + mp[nm] + '\n')
+    f.close()
+
+def main():
+    feature_path = '../data/train_10000.ark'
+    label_path = '../data/train_10000.lab'
+    phone_map_path = '../data/phone_map'
+
+    DATA_SIZE = 10E33
+    X = read_feature(feature_path, DATA_SIZE)
+    Y = read_label(label_path, phone_map_path, DATA_SIZE)
+    # print(X, Y)
+    
+    # orig_path = '/home/step5/MLDS_Data/MLDS_HW1_RELEASE_v1/'
+    # sort_label(orig_path + 'fbank/train.ark', orig_path + 'state_label/train.lab', 
+               # orig_path + 'state_label/train_sorted.lab')
+
+if __name__ == '__main__':
+    main()
+
